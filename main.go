1228553edee275369a5198f9bb5b107c0bff1ad5
Vivesh Yadav <viveshofficial@gmail.com>
2020-03-11 23:22:14 +0530

    Add Function For updateBook

--- main.go
+++ main.go
@@ -69,13 +69,27 @@ func createBook(w http.ResponseWriter ,r *http.Request){
 }
 //updateBook
 func updateBook(w http.ResponseWriter ,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)  
+	for index,item :=range books{            
+		if item.ID == params ["id"] {
+			books = append(books[:index],books[index+1:]...)
+			var book Book
+			_=json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000)) 
+			books = append(books,book)
+			json.NewEncoder(w).Encode(book)
+			
+		}
+	} 
+	json.NewEncoder(w).Encode(books)	
 	
 }
 //deleteBook
 func deleteBook(w http.ResponseWriter ,r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	params := mux.Vars(r)  
-	for index,item :=range books{     // range is used to loop through map,slice ,or any data structure       
+	for index,item :=range books{            
 		if item.ID == params ["id"] {
 			books = append(books[:index],books[index+1:]...)
 			break
